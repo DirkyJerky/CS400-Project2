ab77cb350cdc7e61875817b2129dc44a22769ba5
Jacob Lorenz <58476572+TheExplorativeBadger@users.noreply.github.com>
2020-10-18 17:29:17 -0500

    minor update

--- src/data/api/TwitterRequestObject.java
+++ src/data/api/TwitterRequestObject.java
@@ -14,6 +14,12 @@ public class TwitterRequestObject {
         this.authorizationValue = null;
     }
 
+    public TwitterRequestObject(String url) {
+        this.url = url;
+        this.jsonBody = null;
+        this.authorizationValue = null;
+    }
+
     public TwitterRequestObject(String url, String jsonBody) {
         this.url = url;
         this.jsonBody = jsonBody;
